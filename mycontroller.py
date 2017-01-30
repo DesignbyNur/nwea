200a6a0855d71046a89538414ffa7efd751b3aad
Robert Roy <robr@copiousinc.com>
2017-01-29 23:13:29 -0800

    validate read and write access to db 	modified:   blog.db 	modified:   mycontroller.py 	modified:   testcases

--- mycontroller.py
+++ mycontroller.py
@@ -1,5 +1,7 @@
 import endpoints
 from endpoints import Controller
+import sqlite3
+import json
 
 class Default(Controller):
     def GET(self):
@@ -10,9 +12,23 @@ class Default(Controller):
 
 class Posts(Controller):
     def GET(self):
-        return "/posts GET"
+        conn = sqlite3.connect('blog.db')
+        c = conn.cursor()
+        for row in c.execute('SELECT * FROM posts'):
+            print row 
+        conn.close()
 
 class Post(Controller):
    def POST(self, **kwargs):
-        return 'hello Post {}'.format(kwargs['name'])
+        row = (kwargs['post_id'] , kwargs['title'], kwargs['body'])
+        conn = sqlite3.connect('blog.db')
+        c = conn.cursor()
+        try:
+           c.execute('INSERT INTO posts (post_id,title,body) VALUES(?,?,?)', row ) 
+        except sqlite3.Error as e:
+            print "An error occurred:", e.args[0]
+        conn.commit()
+        conn.close()
+        return 'Posted blog id {}'.format(kwargs['post_id'])
+    
 
